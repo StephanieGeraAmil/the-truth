64cfc8b0eb119ebd6a1029a76f04f7bf0554be07
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2021-10-22 14:55:06 -0300

    fixing bug on setarrayOfVerses

--- src/components/verse.js
+++ src/components/verse.js
@@ -2,8 +2,6 @@ import React,{useEffect} from 'react'
 
 export const Verse = ({verse}) => {
     useEffect(() => {
-        console.log("inside verse")
-        console.log(verse)
      },[]);
     return (
         <div className='card-verse'>
