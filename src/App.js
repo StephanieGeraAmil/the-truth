68eaec0469e45ed422a0f6b29d899c0a0243319f
Stephanie <stephanie.gera.amil@gmail.com>
2022-09-08 13:42:56 -0300

    new tt

--- src/App.js
+++ src/App.js
@@ -1,75 +1,70 @@
-
-import { Box } from './components/box';
-import { Search } from './components/search';
-import './App.css'
-import { Login } from './components/login';
-import { Logout } from './components/logout';
-import { UserInfo } from './components/userInfo';
+import { Box } from "./components/box";
+import { Search } from "./components/search";
+import "./App.css";
+import { Login } from "./components/login";
+import { Logout } from "./components/logout";
+import { UserInfo } from "./components/userInfo";
 import { useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import {getVerses} from './actions/verseActions.js'
-import {getNotes} from './actions/noteActions.js'
-import {getDecks} from './actions/deckActions.js'
-
-
-
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getVerses } from "./actions/verseActions.js";
+import { getNotes } from "./actions/noteActions.js";
+import { getDecks } from "./actions/deckActions.js";
 
 function App() {
-   const dispatch= useDispatch();
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(getVerses());
-      dispatch(getDecks());
-        dispatch(getNotes());
-  
-    },[]);
-   const {isAuthenticated } = useAuth0();
+  useEffect(() => {
+    // dispatch(getVerses());
+    // dispatch(getDecks());
+    // dispatch(getNotes());
+  }, []);
+  // const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
+      {/* <div className="left">
+        <div className="icon-section">
+          {!isAuthenticated && <Login />}
+          {isAuthenticated && (
+            <>
+              <button className="notes_button"></button>
+            </>
+          )}
 
-        <div className='left'>
-         
-              <div className='icon-section'>
-                   {!isAuthenticated && (<Login/>)}
-                     {isAuthenticated && (
-                       <>
-                         
-                          <button className="notes_button"></button>
-                         
-                       </>
-                     )}
-
-                     <div className='user-section'>
-                 
-                     {isAuthenticated && (
-                       <>
-                          <UserInfo/>
-                           <Logout/>
-                       </>
-                     )}
-            
-              </div>
-            
-              </div>
-                 
-
-              <div className='title'>
-                  <h2>The</h2>    
-                  <h1>Truth</h1>
-              </div>
-               <div className='bottom'>
-                    <Search/>
-                    <button className='button-why'>Why</button>
-              </div>
-         
-         
+          <div className="user-section">
+            {isAuthenticated && (
+              <>
+                <UserInfo />
+                <Logout />
+              </>
+            )}
+          </div>
         </div>
-         <div className='right'>
-             <Box/>
+
+        <div className="title">
+          <h2>The</h2>
+          <h1>Truth</h1>
         </div>
-         
-    </div>
+        <div className="bottom">
+          <Search />
+          <button className="button-why">Why</button>
+        </div>
+      </div> */}
+      {/* <div className="right">
+        <Box />
+      </div> */}
+      <div className="lie">
+       <p>I'm thinking...</p>
+       <input></input>
+      <p>but...</p>
+        </div>
+      <div className="truth">
+        <div className="list_verses"></div>
+      </div>
+       {/* <div className="videos_list"></div>
+         <div className="books_list"></div> */}
+      </div>
+  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import './fonts/magnolia_sky.ttf';
-import './fonts/SulphurPoint-Regular.ttf';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "./fonts/magnolia_sky.ttf";
+import "./fonts/SulphurPoint-Regular.ttf";
 import { Auth0Provider } from "@auth0/auth0-react";
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import reducers from './reducers/reducers.js'
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducers from "./reducers/reducers.js";
 
-
-const store= createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
-
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-        <Provider store={store}>
-      <Auth0Provider
-          domain="dev-7pl37pty.us.auth0.com"
-          clientId="vNXWfuyHWr4jF94dV51O4ZclSOpkA8Hw"
-          redirectUri={window.location.origin}
-        >
-        
-                 <App />
-          
-      </Auth0Provider>
-        </Provider>
+    <Provider store={store}>
+      {/* <Auth0Provider
+        domain="dev-7pl37pty.us.auth0.com"
+        clientId="vNXWfuyHWr4jF94dV51O4ZclSOpkA8Hw"
+        redirectUri={window.location.origin}
+      > */}
+        <App />
+      {/* </Auth0Provider> */}
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
