630ca8091dd570b535f94217d5f1bac31d43852e
Stephanie <stephanie.gera.amil@gmail.com>
2022-10-07 12:05:10 -0300

    cleaning up empty spaces

--- src/api/api.js
+++ src/api/api.js
@@ -13,22 +13,17 @@ export const createTag=(newTag)=> axios.post(tags_url, newTag);
 export const updateTag=(updatedTag)=> axios.patch(`${tags_url}/${updatedTag._id}`, updatedTag);
 export const deleteTag=(deleteTagId)=> axios.delete(`${tags_url}/${deleteTagId}`);
 
-
-
 const notes_url= backend_url+"/notes";
 export const fetchNotes=()=>axios.get(notes_url);
 export const createNote=(newNote)=> axios.post(notes_url, newNote);
 export const updateNote=(updatedNote)=> axios.patch(`${notes_url}/${updatedNote._id}`, updatedNote);
 export const deleteNote=(deleteNoteId)=> axios.delete(`${notes_url}/${deleteNoteId}`);
 
-
 const decks_url= backend_url+"/decks";
 export const fetchDecks=()=>axios.get(decks_url);
 export const createDeck=(newDeck)=> axios.post(decks_url, newDeck);
 export const updateDeck=(updatedDeck)=> axios.patch(`${decks_url}/${updatedDeck._id}`, updatedDeck);
 export const deleteDeck=(deleteDeckId)=> axios.delete(`${decks_url}/${deleteDeckId}`);
-
-
 
 const cards_url= backend_url+"/cards";
 export const fetchCards=()=>axios.get(cards_url);
