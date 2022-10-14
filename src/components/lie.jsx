f7834b2e7514738eb7741d54e07c783eb11c8515
Stephanie <stephanie.gera.amil@gmail.com>
2022-10-14 15:19:35 -0300

    changing label to input and adding the logic to show the selected hint if exist

--- src/components/lie.jsx
+++ src/components/lie.jsx
@@ -1,37 +1,35 @@
 import React, { useState, useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { getAllTags } from "../actions/tagActions";
+import { useDispatch, useSelector } from "react-redux";
+import { clearHintSelected } from "../actions/currentSelectionActions";
 
 export const Lie = () => {
-  
+  const dispatch = useDispatch();
+  const selectedSelector = (state) => (state.selected ? state.selected : null);
+  const selected = useSelector(selectedSelector);
+
   const [textInput, setTextInput] = useState("");
-//   const tagsSelector = (state) => (state.tags ? state.tags : null);
-//   const tags = useSelector(tagsSelector);
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(getAllTags());
-//   }, []);
-//   useEffect(() => {
-//     console.log(tags);
-
-//   }, [tags]);
   const handleInputChange = (e) => {
     setTextInput(e.target.value);
+    dispatch(clearHintSelected());
+
     //update the hint list
 
     //the hint list must be ordered by amount of voutes that relate it to the tag
 
     //if it's not in the hint list I should look for it on the phrases/tag table
 
-    //if I don't have nothing I should display a message inviting the user to search 
+    //if I don't have nothing I should display a message inviting the user to search
     //the web or seek counseling and add the information to the site and we must store the search so somebody can look for verses to help future aperances
   };
+  useEffect(() => {
+    const search = selected.hint ? selected.hint.name : "";
+    setTextInput(search);
+  }, [selected]);
 
   return (
     <div className="lie">
       <p>I'm thinking...</p>
-      <label onChange={handleInputChange}>{textInput}</label>
+      <input onChange={handleInputChange} value={textInput}></input>
       <p>but...</p>
     </div>
   );
