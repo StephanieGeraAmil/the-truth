0afd02ccc5758def7f7289b39d95926ef6e7d09b
Stephanie <stephanie.gera.amil@gmail.com>
2022-10-15 15:53:30 -0300

    adding the click outside close suggestion display feature and the enter functionality

--- src/components/lie.jsx
+++ src/components/lie.jsx
@@ -1,42 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { clearHintSelected } from "../actions/currentSelectionActions";
-import { getAllTags } from "../actions/tagActions";
+// import { clearHintSelected } from "../actions/currentSelectionActions";
+// import { getAllTags } from "../actions/tagActions";
 
 export const Lie = () => {
-  const dispatch = useDispatch();
-  // const selectedSelector = (state) => (state.selected ? state.selected : null);
-  // const selected = useSelector(selectedSelector);
+  // const dispatch = useDispatch();
   const tagsSelector = (state) => (state.tags ? state.tags : null);
   const tags = useSelector(tagsSelector);
 
+  const wrapperRef = useRef(null);
+
   const [display, setDisplay] = useState(false);
-  // const [options, setOptions] = useState(tags);
+
   const [textInput, setTextInput] = useState("");
-  // useEffect(() => {
-  //   console.log(tags);
-  //       console.log(tags.map(e=>e.name));
-  //  // setOptions(tags)
-  // }, [tags]);
+
   const handleInputChange = (e) => {
     setTextInput(e.target.value);
-    // setDisplay(true);
-
-    // if(textInput=="")dispatch(clearHintSelected());
-
-    //update the hint list
-
-    //the hint list must be ordered by amount of voutes that relate it to the tag
-
-    //if it's not in the hint list I should look for it on the phrases/tag table
-
-    //if I don't have nothing I should display a message inviting the user to search
-    //the web or seek counseling and add the information to the site and we must store the search so somebody can look for verses to help future aperances
   };
   const handleClickOnSuggestion = (suggestion) => {
     setDisplay(!display);
     setTextInput(suggestion.name);
   };
+
+  const handleKeyPress = (e) => {
+        setDisplay(true);
+    if (e.key === "Enter") {
+      setDisplay(!display);
+    }
+  };
+  const handleClickOutside = (event) => {
+    const {current:wrap} =wrapperRef;
+    if(wrap && !wrap.contains(event.target)){
+    setDisplay(false);
+    }
+
+  };
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  //the hint list must be ordered by amount of voutes that relate it to the tag
+  //if it's not in the hint list I should look for it on thethought/tag table that isn't created yet
+  //if I don't have nothing to suggest I should display a message inviting the user to search the web
+  //or seek counseling and add the information to the site and we must store the search
+  //so somebody can look for verses to help future aperances
 
   return (
     <div className="lie">
@@ -44,23 +54,28 @@ export const Lie = () => {
       <input
         onChange={(event) => handleInputChange(event)}
         onClick={() => setDisplay(!display)}
+        onKeyPress={(e) => handleKeyPress(e)}
         value={textInput}
       ></input>
       {display ? (
-        <div className="suggestions">
+        <div ref={wrapperRef} className="suggestions">
           {tags
-            .filter((element) => (element.name.indexOf(textInput.toLocaleLowerCase())>-1))
-            .map(element => (
+            .filter(
+              (element) =>
+                element.name.indexOf(textInput.toLocaleLowerCase()) > -1
+            )
+            .map((element) => (
               <span
                 key={element.id}
-                onClick={()=>handleClickOnSuggestion(element)}
+                onClick={() => handleClickOnSuggestion(element)}
               >
                 {element.name}
               </span>
-           ))}
+            ))}
         </div>
-      ):<p>but...</p>}
-      
+      ) : (
+        <p>but...</p>
+      )}
     </div>
   );
 };
