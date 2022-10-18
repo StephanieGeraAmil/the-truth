6c9b2c013ace1c94999137d9ef1e3f012c094e43
Stephanie <stephanie.gera.amil@gmail.com>
2022-10-18 14:17:44 -0300

    adding logout button

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 import User from "../assets/user-white.svg";
-import Note from "../assets/notes-white.svg";
+import Log_out from "../assets/logout.svg";
 
 export const Logout = () => {
     const { logout } = useAuth0();
     return (
       <button onClick={() => logout({ returnTo: window.location.origin })}>
-      <img src={Note} alt="logout" />
+      <img src={Log_out} alt="logout" />
     </button>
     )
-    //<button className="logout_button"  onClick={() => logout({ returnTo: window.location.origin })}></button>
+
 }
