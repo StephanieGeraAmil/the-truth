4b426d38c202f471e30b0a3f2160a8ac15c1e93f
Stephanie <stephanie.gera.amil@gmail.com>
2022-10-19 15:22:04 -0300

    formatting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "./fonts/magnolia_sky.ttf";
@@ -21,24 +21,24 @@ const store = createStore(
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-    <Provider store={store}>
-      <Auth0Provider
-        domain="dev-7pl37pty.us.auth0.com"
-        clientId="vNXWfuyHWr4jF94dV51O4ZclSOpkA8Hw"
-        redirectUri={window.location.origin}
-      >
-         <Routes>
-                <Route path="/" element={<App />}>  </Route>
-                      <Route path="/decks" element={<DeckDashboard />}> </Route>
-                          <Route path="/decks/:id" element={<Deck/>} />
-    
-                     
-             
+    <BrowserRouter>
+      <Provider store={store}>
+        <Auth0Provider
+          domain="dev-7pl37pty.us.auth0.com"
+          clientId="vNXWfuyHWr4jF94dV51O4ZclSOpkA8Hw"
+          redirectUri={window.location.origin}
+        >
+          <Routes>
+            <Route path="/" element={<App />}>
+              {" "}
+            </Route>
+            <Route path="/decks" element={<DeckDashboard />}>
+              {" "}
+            </Route>
+            <Route path="/decks/:id" element={<Deck />} />
           </Routes>
-   
-      </Auth0Provider>
-    </Provider>
+        </Auth0Provider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
