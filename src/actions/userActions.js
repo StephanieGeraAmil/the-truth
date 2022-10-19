eab862ed7e1517585ab461f74000dcbae13cd09a
Stephanie <stephanie.gera.amil@gmail.com>
2022-10-19 13:16:55 -0300

    removing logs

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -14,13 +14,9 @@ export const getUserByEmail = (user) => async (dispatch, getState) => {
   try {
     const { data } = await api.fetchUserByEmail(user);
     if (data!="") {
-                  console.log("found")
-    
       const action = { type: actions.FETCH_USER_BY_EMAIL, payload: data };
       dispatch(action);
     } else {
-  
-            console.log("not found")
       const { data } = await api.createUser(user);
       const action = { type: actions.CREATE_USER, payload: data };
       dispatch(action);
