ec2777c69ea17d243c1dfa96d0a978fd448c077b
Stephanie <stephanie.gera.amil@gmail.com>
2022-11-08 20:13:26 -0300

    adding styled components to the truth section

--- src/components/truth.jsx
+++ src/components/truth.jsx
@@ -2,6 +2,29 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Verse } from "./verse";
 
+import styled, { css } from "styled-components";
+
+
+const TruthContainer = styled.div`
+  box-sizing: border-box;
+  background-color: rgb(138, 175, 205);
+  border-radius: 10px;
+  padding: 15px;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+`;
+const ListVersesContainer = styled.div`
+  background-color: #fff;
+  height: 300px;
+
+  border-radius: inherit;
+  overflow-y: scroll;
+`;
+
+const TruthParagraph = styled.p`
+  margin-top: 0;
+`;
 
 export const Truth = () => {
   const versesSelector = (state) => (state.verses ? state.verses : null);
@@ -12,13 +35,13 @@ export const Truth = () => {
   }, []);
 
   return (
-    <div className="truth">
-      <p>God says...</p>
-      <div className="list_verses">
+    <TruthContainer>
+      <TruthParagraph>God says...</TruthParagraph>
+      <ListVersesContainer>
         {versesOfTag.map((element) => (
             <Verse verse={element} key={`${element.id}_div`} />
         ))}
-      </div>
-    </div>
+      </ListVersesContainer>
+    </TruthContainer>
   );
 };
--- src/components/verse.jsx
+++ src/components/verse.jsx
@@ -1,24 +1,45 @@
 import React, { useEffect } from "react";
 import Plus from "../assets/plus.svg";
 
+import styled, { css } from "styled-components";
+import { Paragraph } from "./shared_styles/styled_content_components";
+import { StyledButton } from "./shared_styles/styled_buttons";
+
+const VerseListItemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const VerseContainer = styled.div`
+ padding: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  color: grey;
+`;
+const VerseRef = styled.p`
+ align-self: flex-end;
+  color: darkgrey;
+  `;
+
 export const Verse = ({ verse }) => {
   useEffect(() => {}, []);
   return (
-    <div className="verse_list_item" >
-      <div className="verse">
-        <p>{verse.scripture}</p>
-        <p className="ref">
+    <VerseListItemContainer>
+      <VerseContainer>
+        <Paragraph>{verse.scripture}</Paragraph>
+        <VerseRef>
           {verse.book.charAt(0).toUpperCase() +
             verse.book.slice(1) +
             " " +
             verse.chapter +
             ":" +
             verse.verse_number}
-        </p>
-      </div>
-      <button className="page_button"> 
+        </VerseRef>
+      </VerseContainer>
+      <StyledButton className="page_button"> 
         <img src={Plus} alt="add_to_deck" />
-      </button>
-    </div>
+      </StyledButton>
+    </VerseListItemContainer>
   );
 };
