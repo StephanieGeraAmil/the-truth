8c780a0c4efd45d39dd4eb8d566b6ea5870c136a
Stephanie <stephanie.gera.amil@gmail.com>
2023-04-28 12:58:54 -0300

    adding the button to save the verse on a deck

--- src/components/truth.jsx
+++ src/components/truth.jsx
@@ -1,11 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import { Verse } from "./verse";
-
 import styled from "styled-components";
 import { StyledCard } from "./shared_styles/styled_cards";
+import { RiAddBoxFill } from "react-icons/ri";
+export const StyledButton = styled.button`
+position:absolute;
+  top:2vh ;
+  right:1vh;
+  z-index: 0;
+  border: 0;
+  background: transparent;
+  align-self: flex-end;
+`;
 
+const TruthCard = styled(StyledCard)`
+position:relative;
+  box-shadow: 0.3vw 0.3vw 0.5vw 0.08vw #888;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 2vw;
+  height: 20vh;
+  min-width: 40vh;
+  max-width: 30vw;
+
+  @media (max-width: 500px) {
+    min-height: 35vh;
+    min-width: 80vw;
+    width: 90%;
+    margin: auto;
+  }
+`;
 const TruthContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -16,34 +43,13 @@ const TruthContainer = styled.div`
   @media (max-width: 500px) {
     flex-direction: column;
     align-items: center;
-    justify-content:flex-start;
+    justify-content: flex-start;
     gap: 3vh;
     height: 80vh;
-    margin:auto;
+    margin: auto;
     margin-top: 5vh;
-    width:100%;
-
+    width: 100%;
   }
-`;
-
-const TruthCard = styled(StyledCard)`
-  box-shadow: 0.3vw 0.3vw 0.5vw 0.08vw #888;
-  display:flex;
-  flex-direction:row;
-  justify-content:center;
-  align-items:center;
-  border-radius: 2vw;
-  height: 20vh;
-  min-width: 40vh; 
-   max-width: 30vw; 
-
-  @media (max-width: 500px) {
-    min-height: 35vh;
-      min-width: 80vw; 
-      width:90%;
-        margin: auto;
-  }
-
 `;
 
 export const Truth = () => {
@@ -55,27 +61,19 @@ export const Truth = () => {
       {versesRelated &&
         versesRelated.map((element) => (
           <TruthCard key={element.ref}>
+            <StyledButton
+              transparent
+              onClick={() => {
+                return;  
+                //ask whick deck should it be added to
+               // dispatch(addResourceToDeck(element,deck));
+              }}
+            >
+              <RiAddBoxFill style={{ color: "#0D0C3C", fontSize: "2vh" }} />
+            </StyledButton>
             <Verse verse={element} />
           </TruthCard>
         ))}
-      {/* <>
-            <TruthCard key={"2"}>
-            </TruthCard>
-            <TruthCard key={"3"}>
-            </TruthCard>
-            <TruthCard key={"4"}>
-            </TruthCard>
-            <TruthCard key={"5"}> 
-            </TruthCard>
-            <TruthCard key={"6"}>
-            </TruthCard>
-            <TruthCard key={"7"}>
-            </TruthCard>
-            <TruthCard key={"8"}>
-            </TruthCard>
-            <TruthCard key={"9"}>
-            </TruthCard>
-          </> */}
     </TruthContainer>
   );
 };
