a259ad89ac2a51df0076eee70481bd7ad1c114b2
Stephanie <stephanie.gera.amil@gmail.com>
2023-05-03 10:10:27 -0300

    deleting unnecesary dispatch of action

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,6 @@ import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 import { getUserByEmail } from "./../actions/userActions.js";
-import { getDecksOfUser } from "./../actions/deckActions.js";
 import {FaSearch,FaClone,FaCaretLeft,FaPen} from "react-icons/fa";
 import { Login } from "./login";
 import { Logout } from "./logout";
@@ -41,14 +40,12 @@ export const Nav = () => {
   return (
     <NavContainer>
      {location.pathname!=="/decks"&& <StyledLink
-        onClick={() => dispatch(getDecksOfUser("userLogged"))}
         to="/decks"
         
       >
         <img src={Deck} alt="deck_page" style={{ color:"#8B8C89",height:  "4vh"}} />
       </StyledLink>}
       {location.pathname!=="/"&& <StyledLink
-        onClick={() => dispatch(getDecksOfUser("userLogged"))}
         to="/"
       >
         <FaSearch style={{color: '#8B8C89', fontSize: "3.5vh" }}/>
