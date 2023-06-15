c40141e56ae1128966c94eba3dfdd963d5d1330f
Stephanie <stephanie.gera.amil@gmail.com>
2023-06-15 16:59:15 -0300

    fixing truth cards and adding references to the verses

--- src/components/shared_styles/styled_text.js
+++ src/components/shared_styles/styled_text.js
@@ -28,19 +28,31 @@ export const SubTitle = styled.h3`
     props.color &&
     css`
       /* color: #5aa2f0; */
-          color:#33ABB9
+          color:#33ABB9;
     `}
+    
 `;
 export const Info = styled.p`
   font-size: 2vh;
   line-height: 1.1em;
   font-weight: 100;
-  margin: 0;
   width: 80%;
+    margin: 0 auto;
   align-self: flex-start;
   /* margin-bottom: 0.5em; */
   padding: 0;
   color: #fff;
+   ${(props) =>
+    props.gray &&
+    css`
+      /* color: #5aa2f0; */
+          color:#000;
+    `}
+     ${(props) =>
+    props.bold &&
+    css`
+          font-weight:500;
+    `}
   @media (max-width: 1800px) and (min-height: 1000px) {
     font-size: 1rem;
   }
--- src/components/truth.jsx
+++ src/components/truth.jsx
@@ -11,10 +11,10 @@ import { TruthCard } from "./truthCard";
 import styled from "styled-components";
 import { StyledCard } from "./shared_styles/styled_cards";
 import { Form, FormInput } from "./shared_styles/styled_forms";
+import { Info } from "./shared_styles/styled_text";
 import { RiAddBoxFill } from "react-icons/ri";
 import { FiPlusCircle } from "react-icons/fi";
 import { MdArrowBack, MdOutlineDone } from "react-icons/md";
-
 
 // export const CloseButton = styled.button`
 //   position: absolute;
@@ -45,12 +45,12 @@ import { MdArrowBack, MdOutlineDone } from "react-icons/md";
 const TruthContainer = styled.div`
   /* position: absolute;
   bottom: 0; */
-   position: relative;
-   align-self:flex-start;
-  height:70%;
+  position: relative;
+  align-self: flex-start;
+  height: 70%;
 
   width: 90%;
-  margin:0 auto;
+  margin: 0 auto;
   z-index: 0;
   /* background: #8b8c89;
   box-shadow: 6px 5px 16px #000; */
@@ -58,14 +58,14 @@ const TruthContainer = styled.div`
   /* display: flex;
   flex-direction: column;
   justify-content: center; */
-overflow:auto;
+  overflow: auto;
   display: grid;
   /* flex-direction: row;
   flex-wrap: wrap; */
-    grid-gap: 2%;
+  grid-gap: 2%;
   /* grid-auto-flow: row; */
-  grid-template-columns: repeat( auto-fill,minmax(200px,1fr) );
-    grid-auto-rows: 230px;
+  grid-template-columns: repeat(auto-fill, minmax(30vh, 1fr));
+  grid-auto-rows: 40vh;
   /* justify-content: flex-start;
   align-items: center; */
 
@@ -82,7 +82,6 @@ export const Truth = () => {
   const versesSelector = (state) => (state.verses ? state.verses : null);
   const versesRelated = useSelector(versesSelector);
   const wrapperRef = useRef(null);
-
 
   const handleClickOutside = (event) => {
     const { current: wrap } = wrapperRef;
@@ -102,15 +101,19 @@ export const Truth = () => {
       {/* <CloseButton transparent onClick={() => dispatch(clearThoughtSelected())}>
         <MdArrowBack style={{ color: "#1E1D25", fontSize: "4vh" }} />
       </CloseButton> */}
-      
+
       {/* <Truthlist> */}
-        {versesRelated &&
-          versesRelated.map((element) => (
-              <PreviewCard  static key={element.ref}>{element ? element.scripture : ""}</PreviewCard>
-            // <TruthCard key={element.ref} verse={element}>
-            // </TruthCard>
-          ))}
-     
+      {versesRelated &&
+        versesRelated.map((element) => (
+          <PreviewCard static key={element.ref}>
+            <>
+              <Info gray>{element ? element.scripture : ""}</Info>
+              <Info gray bold>{element ? element.ref : ""}</Info>
+            </>
+          </PreviewCard>
+          // <TruthCard key={element.ref} verse={element}>
+          // </TruthCard>
+        ))}
 
       {/* </Truthlist> */}
     </TruthContainer>
